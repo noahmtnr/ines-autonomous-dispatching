08eef463beba8ec6caef16a222be80ee8d13e28b
Noah <noah.mautner@outlook.de>
2022-04-02 11:09:35 +0200

    make preprocessing class statically usable everywhere

--- preprocessing/data_manipulation.py
+++ preprocessing/data_manipulation.py
@@ -4,19 +4,18 @@ from data_preprocessing import DataPreProcessing
 
 
 def generate_timestamps_for_route():
-    graph = ox.io.load_graphml("../data/graph/full.graphml")
-    trips = pd.read_csv("../data/trips/trips_with_nodes.csv")
-    data_processing = DataPreProcessing(graph, trips)
-    trips_with_timestamps = data_processing.map_routes_to_trips_with_timestamps()
-    trips_with_timestamps.to_csv("../data/trips/trips_with_routes_timestamps.csv")
+    graph = ox.io.load_graphml("data/graph/full.graphml")
+    trips = pd.read_csv("data/trips/trips_with_nodes.csv")
+    trips_with_timestamps = DataPreProcessing.map_routes_to_trips_with_timestamps(trips, graph)
+    trips_with_timestamps.to_csv("data/trips/trips_with_routes_timestamps.csv")
 
 
 def generate_route_for_trips():
-    graph = ox.io.load_graphml("../data/graph/full.graphml")
-    trips = pd.read_csv("../data/trips/trips_with_nodes.csv")
-    data_processing = DataPreProcessing(graph, trips)
-    trips_with_routes = data_processing.map_routes_to_trips()
-    trips_with_routes.to_csv("../data/trips/trips_with_routes.csv")
+    graph = ox.io.load_graphml("data/graph/full.graphml")
+    trips = pd.read_csv("data/trips/trips_with_nodes.csv")
+    
+    trips_with_routes = DataPreProcessing.map_routes_to_trips(trips, graph)
+    trips_with_routes.to_csv("data/trips/trips_with_routes.csv")
 
 generate_timestamps_for_route()
 generate_route_for_trips()
