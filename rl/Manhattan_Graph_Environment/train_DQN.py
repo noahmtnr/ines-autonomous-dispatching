03e92da44a597df044b5d62fb468d9295e8ea105
Noah <noah.mautner@outlook.de>
2022-05-26 13:37:24 +0200

    working dqn training after merge

--- rl/Manhattan_Graph_Environment/train_DQN.py
+++ rl/Manhattan_Graph_Environment/train_DQN.py
@@ -41,6 +41,8 @@ trainer = DQNTrainer(trainer_config,GraphEnv )
 #Define the path where the results of the trainer should be saved
 checkpoint_root = "tmp/dqn/graphworld"
 shutil.rmtree(checkpoint_root, ignore_errors=True, onerror=None)   # clean up old runs
+ray_results = "{}/ray_results/".format(os.getenv("HOME"))
+shutil.rmtree(ray_results, ignore_errors=True, onerror=None)   # clean up old runs
 
 
 # Run trainer
@@ -53,46 +55,44 @@ n_iter = 1
 for n in range(n_iter):
     result = trainer.train()
     results.append(result)
-    print("TEST", n)
-    print(result)
+    print("Episode", n)
 
     episode = {'n': n,
-               'n_trained_episodes': result['episodes_this_iter'],
-               'episode_reward_min': result['episode_reward_min'],
-               'episode_reward_mean': result['episode_reward_mean'],
-               'episode_reward_max': result['episode_reward_max'],
-               'episode_len_mean': result['episode_len_mean'],
-               'count_wait_min': result["count_wait_min"],
-               'count_wait_max': result["count_wait_max"],
-               'count_wait_mean': result["count_wait_mean"],
-               'count_bookown_min': result["count_bookown_min"],
-               'count_bookown_max': result["count_bookown_max"],
-               'count_bookown_mean': result["count_bookown_mean"],
-               'count_share_min': result["count_share_min"],
-               'count_share_max': result["count_share_max"],
-               'count_share_mean': result["count_share_mean"],
-               'share_wait_min': result["share_wait_min"],
-               'share_wait_max': result["share_wait_max"],
-               'share_wait_mean': result["share_wait_mean"],
-               'share_bookown_min': result["share_bookown_min"],
-               'share_bookown_max': result["share_bookown_max"],
-               'share_bookown_mean': result["share_bookown_mean"],
-               'share_share_min': result["share_share_min"],
-               'share_share_max': result["share_share_max"],
-               'share_share_mean': result["share_share_mean"],
+               'n_trained_episodes': int(result['episodes_this_iter']),
+               'episode_reward_min': float(result['episode_reward_min']),
+               'episode_reward_mean': float(result['episode_reward_mean']),
+               'episode_reward_max': float(result['episode_reward_max']),
+               'episode_len_mean': float(result['episode_len_mean']),
+               'count_wait_min': int(result["count_wait_min"]),
+               'count_wait_max': int(result["count_wait_max"]),
+               'count_wait_mean': float(result["count_wait_mean"]),
+               'count_bookown_min': int(result["count_bookown_min"]),
+               'count_bookown_max': int(result["count_bookown_max"]),
+               'count_bookown_mean': float(result["count_bookown_mean"]),
+               'count_share_min': int(result["count_share_min"]),
+               'count_share_max': int(result["count_share_max"]),
+               'count_share_mean': float(result["count_share_mean"]),
+               'share_wait_min': float(result["share_wait_min"]),
+               'share_wait_max': float(result["share_wait_max"]),
+               'share_wait_mean': float(result["share_wait_mean"]),
+               'share_bookown_min': float(result["share_bookown_min"]),
+               'share_bookown_max': float(result["share_bookown_max"]),
+               'share_bookown_mean': float(result["share_bookown_mean"]),
+               'share_share_min': float(result["share_share_min"]),
+               'share_share_max': float(result["share_share_max"]),
+               'share_share_mean': float(result["share_share_mean"]),
                }
-    print(type(episode))
     episode_data.append(episode)
     episode_json.append(json.dumps(episode))
     file_name = trainer.save(checkpoint_root)
     wandb.log({"n_trained_episodes": result['episodes_this_iter'],
                 "mean_reward": result['episode_reward_mean'],
-                "mean_reward": result['episode_reward_mean'],
+                "max_reward": result['episode_reward_max'],
                 "own_mean": result['count_bookown_mean'],
                 "wait_mean": result['count_wait_mean'],
                 "share_mean": result['count_share_mean'],
-                "share_of_own_mean": result['share_bookown_mean'],
-                "share_of_wair_mean": result['share_wait_mean'],
+                "share_of_bookown_mean": result['share_bookown_mean'],
+                "share_of_wait_mean": result['share_wait_mean'],
                 "share_of_share_mean": result['share_share_mean'],
     })
 
--- rl/Manhattan_Graph_Environment/train_PPO.py
+++ rl/Manhattan_Graph_Environment/train_PPO.py
@@ -15,7 +15,7 @@ from ray.rllib.agents.ppo import PPOTrainer, DEFAULT_CONFIG
 sys.path.insert(0,"")
 
 from ManhattanGraph import ManhattanGraph
-from gym_graphenv.envs.GraphworldManhattan import GraphEnv
+from gym_graphenv.envs.GraphworldManhattan import GraphEnv, CustomCallbacks
 
 wandb.login(key="93aab2bcc48447dd2e8f74124d0258be2bf93859")
 wandb.init(project="Manhattan-ppo", entity="hitchhike")
@@ -51,7 +51,7 @@ shutil.rmtree(ray_results, ignore_errors=True, onerror=None)   # clean up old ru
 results = []
 episode_data = []
 episode_json = []
-n_iter = 3
+n_iter = 1
 
 for n in range(n_iter):
     result = trainer.train()
@@ -59,15 +59,42 @@ for n in range(n_iter):
     print("TEST", n)
 
     episode = {'n': n,
-               'episode_reward_min': result['episode_reward_min'],
-               'episode_reward_mean': result['episode_reward_mean'],
-               'episode_reward_max': result['episode_reward_max'],
-               'episode_len_mean': result['episode_len_mean'],
+               'n_trained_episodes': int(result['episodes_this_iter']),
+               'episode_reward_min': float(result['episode_reward_min']),
+               'episode_reward_mean': float(result['episode_reward_mean']),
+               'episode_reward_max': float(result['episode_reward_max']),
+               'episode_len_mean': float(result['episode_len_mean']),
+               'count_wait_min': int(result["count_wait_min"]),
+               'count_wait_max': int(result["count_wait_max"]),
+               'count_wait_mean': float(result["count_wait_mean"]),
+               'count_bookown_min': int(result["count_bookown_min"]),
+               'count_bookown_max': int(result["count_bookown_max"]),
+               'count_bookown_mean': float(result["count_bookown_mean"]),
+               'count_share_min': int(result["count_share_min"]),
+               'count_share_max': int(result["count_share_max"]),
+               'count_share_mean': float(result["count_share_mean"]),
+               'share_wait_min': float(result["share_wait_min"]),
+               'share_wait_max': float(result["share_wait_max"]),
+               'share_wait_mean': float(result["share_wait_mean"]),
+               'share_bookown_min': float(result["share_bookown_min"]),
+               'share_bookown_max': float(result["share_bookown_max"]),
+               'share_bookown_mean': float(result["share_bookown_mean"]),
+               'share_share_min': float(result["share_share_min"]),
+               'share_share_max': float(result["share_share_max"]),
+               'share_share_mean': float(result["share_share_mean"]),
                }
-
     episode_data.append(episode)
     episode_json.append(json.dumps(episode))
     file_name = trainer.save(checkpoint_root)
-    wandb.log({"mean_reward": result['episode_reward_mean']})
+    wandb.log({"n_trained_episodes": result['episodes_this_iter'],
+                "mean_reward": result['episode_reward_mean'],
+                "max_reward": result['episode_reward_max'],
+                "own_mean": result['count_bookown_mean'],
+                "wait_mean": result['count_wait_mean'],
+                "share_mean": result['count_share_mean'],
+                "share_of_bookown_mean": result['share_bookown_mean'],
+                "share_of_wait_mean": result['share_wait_mean'],
+                "share_of_share_mean": result['share_share_mean'],
+    })
 
     print(f'{n + 1:3d}: Min/Mean/Max reward: {result["episode_reward_min"]:8.4f}/{result["episode_reward_mean"]:8.4f}/{result["episode_reward_max"]:8.4f}, len mean: {result["episode_len_mean"]:8.4f}. Checkpoint saved to {file_name}')
