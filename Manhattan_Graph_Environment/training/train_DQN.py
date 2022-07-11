b145cad7cbc741417cb8853eddf68640f677fc1a
qm2021-lkirchdo <90602255+qm2021-lkirchdo@users.noreply.github.com>
2022-07-11 12:01:53 +0200

    DQN train changes

--- Manhattan_Graph_Environment/training/train_DQN.py
+++ Manhattan_Graph_Environment/training/train_DQN.py
@@ -23,7 +23,7 @@ from Manhattan_Graph_Environment.graphs.ManhattanGraph import ManhattanGraph
 from Manhattan_Graph_Environment.gym_graphenv.envs.GraphworldManhattan import GraphEnv, CustomCallbacks
 
 wandb.login(key="93aab2bcc48447dd2e8f74124d0258be2bf93859")
-wandb.init(project="Manhattan-DQN", entity="hitchhike")
+wandb.init(project="Comparison-Total_Env", entity="hitchhike")
 
 env=GraphEnv()
 
@@ -37,7 +37,7 @@ trainer_config['num_workers'] = 1
 trainer_config["train_batch_size"] = 400
 trainer_config["gamma"] = 0.95
 trainer_config["n_step"] = 1
-trainer_config["framework"] = "torch"
+#trainer_config["framework"] = "torch"
 trainer_config["callbacks"] = CustomCallbacks
 #trainer_config["num_gpus"] = int(os.environ.get("RLLIB_NUM_GPUS", "0"))
 trainer_config["explore"] = True
@@ -63,7 +63,7 @@ shutil.rmtree(ray_results, ignore_errors=True, onerror=None)   # clean up old ru
 results = []
 episode_data = []
 episode_json = []
-n_iter = 300
+n_iter = 100
 for n in range(n_iter):
     result = trainer.train()
     results.append(result)
