1bc1640af61255e9bd675f14f63100ebbfa86499
Noah <noah.mautner@outlook.de>
2022-08-06 11:27:07 +0200

    cleaned up and commented

--- Manhattan_Graph_Environment/database_connection.py
+++ Manhattan_Graph_Environment/database_connection.py
@@ -28,43 +28,15 @@ class DBConnection:
         print("Using remote db")
         self.mycursor = self.mydb.cursor()
 
-    def initialiazeTables(self):
-
-        self.mycursor.execute(
-            """ create table TRIPS (
-        id varchar(30) primary key,
-        vendor_id int,
-        pickup_datetime timestamp,
-        dropoff_datetime timestamp,
-        passenger_count int,
-        pickup_longitude float,
-        pickup_latitude float,
-        dropoff_longitude float,
-        dropoff_latitude float,
-        store_and_fwd_flag varchar(2),
-        trip_duration int,
-        pickup_node bigint,
-        dropoff_node bigint,
-        pickup_distance float,
-        dropoff_distance float,
-        route_length float,
-        provider int,
-        total_price float)""")
-
-        self.mycursor.execute("""
-        create table TRIPS_ROUTES (
-          id varchar(30),
-          route_node bigint,
-          date_time timestamp,
-          foreign key (id) references TRIPS(id),
-          primary key (id, route_node, date_time)
-          )
-          """)
-
     def insertIntoTripsRoutes(self, tripId, listTrips):
+        """ Helper function to fill trips_routes table
+
+        Args:
+            tripId (_type_): _description_
+            listTrips (_type_): _description_
+        """
         for element in listTrips.split(', '):
             a, b = element.split(' : ')
-            # new_b = datetime.fromisoformat(b)
             timestamp_b = datetime.strptime(str(b), "'%Y-%m-%d %H:%M:%S'")
             sql = "INSERT INTO TRIPS_ROUTES VALUES (%s, %s, %s)"
             val = (tripId, a, timestamp_b)
@@ -75,6 +47,8 @@ class DBConnection:
                         pickup_latitude, dropoff_longitude, dropoff_latitude, store_and_fwd_flag, trip_duration,
                         pickup_node, dropoff_node, pickup_distance, dropoff_distance, route_length, provider,
                         total_price):
+        """ Helper function to write trip into trips table"""
+
         sql = "INSERT INTO TRIPS VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
         val = (id, vendor_id, pickup_datetime, dropoff_datetime, passenger_count, pickup_longitude, pickup_latitude,
                dropoff_longitude, dropoff_latitude, store_and_fwd_flag, trip_duration, pickup_node, dropoff_node,
@@ -83,6 +57,15 @@ class DBConnection:
         self.mydb.commit()
 
     def getAvailableTripsAtNode(self, start_node, start_date, end_date):
+        """Fetches all available trips at a node for a given time window defined by start and end timestamps. Still missing a mapping function that maps the query to the correct partition of the prefiltered_trips_view. Currently only a few 2 weeks windows can be queried (see README>Database).
+
+        Args:
+            start_date (String): lower time boundary to query for
+            end_date (String): upper time boundary to query for
+
+        Returns:
+            list: available trips
+        """
         startTime = time.time()
         sql = "select * from PREFILTERED_TRIPS_VIEW_01 where route_node = %s and date_time between %s and %s"
         val = (start_node, start_date, end_date)
@@ -97,6 +80,15 @@ class DBConnection:
         return result
 
     def getAvailableTrips(self, start_date, end_date):
+        """Fetches all available trips for a given time window defined by start and end timestamps. Still missing a mapping function that maps the query to the correct partition of the prefiltered_trips_view. Currently only a few 2 weeks windows can be queried (see README>Database).
+
+        Args:
+            start_date (String): lower time boundary to query for
+            end_date (String): upper time boundary to query for
+
+        Returns:
+            list: available trips
+        """
         startTime = time.time()
         sql = "select * from PREFILTERED_TRIPS_VIEW_01 where date_time between %s and %s"
         val = (start_date, end_date)
@@ -112,6 +104,14 @@ class DBConnection:
         return result
 
     def getRouteFromTrip(self, trip_id):
+        """Gets detailed route information for a given trip_id like pickup, dropoff coordinates, passenger count, total price.
+
+        Args:
+            trip_id (int)
+
+        Returns:
+            _type_: _description_
+        """
         startTime = time.time()
         sql = "select route_node, date_time from mannheimprojekt.TRIPS_ROUTES where id = %s order by date_time"
         val = (trip_id,)
@@ -126,6 +126,8 @@ class DBConnection:
         return nodes_list, time_list
 
     def populateDatabase(self):
+        """Reads raw trips from file and writes them into trips table
+        """
         self.initialiazeTables()
         full_df = pd.read_csv('rl/Graph_Environment/trips_kaggle_providers.csv')
 
@@ -151,6 +153,11 @@ class DBConnection:
         rest_df.apply(lambda row: self.insertIntoTripsRoutes(row['id'], row['route_timestamps']), axis=1)
 
     def writeHubsToDB(self, hubs):
+        """Helper function to write into hubs table.
+
+        Args:
+            hubs (list(int)): List of hub ids
+        """
         sql = "INSERT INTO HUBS VALUES ( %s )"
         for hub in hubs:
             values = (int(hub),)
@@ -159,6 +166,11 @@ class DBConnection:
         self.mydb.commit()
 
     def getAllHubs(self):
+        """Retrieves all hubs
+
+        Returns:
+            list(int): list of hub ids
+        """
         sql = "SELECT * FROM HUBS ORDER BY id ASC"
 
         self.mycursor.execute(sql)
